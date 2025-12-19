e7ad6edb234fd73169132a5c3019f11ff446e93a
Aiden Cullo <aidencullo@gmail.com>
2025-12-19 16:50:24 -0500

    Add Docker Machine environment setup batch file

---
+++ docker-env.bat
@@ -0,0 +1,6 @@
+@echo off
+set userprofile=%USERPROFILE%
+set docker_cert_path=%userprofile%\.docker\machine\machines\default
+set docker_host=tcp://192.168.99.100
+set docker_machine_name=default
+set docker_tls_verify=1
